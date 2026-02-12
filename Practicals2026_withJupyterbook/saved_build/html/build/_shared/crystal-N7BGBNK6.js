4edd4678aaed19f4f69ddd6d4597d2ac68b1f358
martin <martin.colot@ulb.be>
2026-02-12 16:56:17 +0100

    build

--- Practicals2026_withJupyterbook/saved_build/html/build/_shared/crystal-N7BGBNK6.js
+++ Practicals2026_withJupyterbook/saved_build/html/build/_shared/crystal-N7BGBNK6.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-DORBKLGC.js";import"/build/_shared/chunk-KPSTTWTG.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/myst_assets_folder/_shared/chunk-DORBKLGC.js";import"/myst_assets_folder/_shared/chunk-KPSTTWTG.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
--- Practicals2026_withJupyterbook/saved_build/html/build/_shared/django-OWTAEXTI.js
+++ Practicals2026_withJupyterbook/saved_build/html/build/_shared/django-OWTAEXTI.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-GHUALMRT.js";import"/build/_shared/chunk-J3PKFMLQ.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/myst_assets_folder/_shared/chunk-GHUALMRT.js";import"/myst_assets_folder/_shared/chunk-J3PKFMLQ.js";import"/myst_assets_folder/_shared/chunk-OZE3FFNP.js";export default a();
